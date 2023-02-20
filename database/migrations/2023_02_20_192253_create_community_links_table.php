096c52b0ae006ce5aa3290827964b0d4d44836fc
Javi <javiermespino@gmail.com>
2023-02-20 22:55:50 +0000

    Laravel-A02

---
+++ database/migrations/2023_02_20_192253_create_community_links_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('community_links', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id')->index();
+            $table->integer('channel_id')->index();
+            $table->string('title');
+            $table->string('link')->unique();
+            $table->boolean('approved')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('community_links');
+    }
+};
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes(['verify' => 'true']);
+
+Route::group(['middleware' => 'verified'], function () {
+
+    // Rutas a verificar
+
+});
